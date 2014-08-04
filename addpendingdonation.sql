124356c23860e25e054287d615e7918fcec7d5f1
Dave Bergschneider <Dave Bergschneider@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-04 17:17:24 +0000

    Missing file
    git-svn-id: https://plugins.svn.wordpress.org/blackbaud-sphere/trunk@960089 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addpendingdonation.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `wpgu34buyn_2_supporters` ADD `pending_donations` DECIMAL(10,2) NOT NULL AFTER `amount_raised`;
+ALTER TABLE `wpgu34buyn_3_supporters` ADD `pending_donations` DECIMAL(10,2) NOT NULL AFTER `amount_raised`;
+ALTER TABLE `wpgu34buyn_4_supporters` ADD `pending_donations` DECIMAL(10,2) NOT NULL AFTER `amount_raised`;
+ALTER TABLE `wpgu34buyn_supporters` ADD `pending_donations` DECIMAL(10,2) NOT NULL AFTER `amount_raised`;
